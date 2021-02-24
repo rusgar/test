81087e02ce69209f72fddb3be025b7127f1b2f3d
rusgar <edamamon@gmail.com>
2021-02-24 16:03:39 +0100

    Rutas

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+<<<<<<< Updated upstream
 import TestInicio from '@/components/TestInicio'
+=======
+import TestComponent from '@/components/TestComponent.vue'
+>>>>>>> Stashed changes
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
@@ -12,6 +16,7 @@ describe('HelloWorld.vue', () => {
   })
 })
 
+<<<<<<< Updated upstream
 describe('TestInicio.vue', () => {
   it('deberia pasar si se renderizan los slots', () => {
     const title = '<span>Title</span>'
@@ -21,3 +26,14 @@ describe('TestInicio.vue', () => {
     expect(wrapper.html()).toMatch(title)
   })
 })
+=======
+describe('TestComponent.vue', () => {
+  it('should pass when rendering slots', () => {
+    const title = '<span>Title</span>'
+    const wrapper = shallowMount(TestComponent, {
+      slots: { title: title }
+    })
+    expect(wrapper.html()).toMatch(title)
+  })
+})
+>>>>>>> Stashed changes
